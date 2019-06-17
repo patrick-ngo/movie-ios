26eb14fe254953c3cf1b6265badfc03fa6ac05cd
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-17 22:57:32 +0800

    create the main reducer to combine the other reducers

---
+++ movie-ios/Reducers/MainReducer.swift
@@ -0,0 +1,32 @@
+//
+//  MainReducer.swift
+//  movie-ios
+//
+//  Created by Patrick Ngo on 2019-06-17.
+//  Copyright © 2019 Patrick Ngo. All rights reserved.
+//
+
+import ReSwift
+import ReSwiftThunk
+
+struct AppState: StateType {
+    var movieListState: MovieListState = MovieListState()
+    var movieDetailState: MovieDetailState = MovieDetailState()
+}
+
+
+func appReducer(action: Action, state: AppState?) -> AppState {
+    return AppState(
+        movieListState: movieListReducer(action: action, state: state?.movieListState),
+        movieDetailState: movieDetailReducer(action: action, state: state?.movieDetailState)
+    )
+}
+
+let thunksMiddleware: Middleware<AppState> = createThunksMiddleware()
+
+let mainStore = Store(
+    reducer: appReducer,
+    state: AppState(),
+    middleware: [thunksMiddleware]
+)
+
