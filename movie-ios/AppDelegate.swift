35616d5b3b21e4c984fd64a0891058de6f5952d0
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-17 22:27:41 +0800

    set initial rioot ViewController

--- movie-ios/AppDelegate.swift
+++ movie-ios/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        
+        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
         return true
     }
 
