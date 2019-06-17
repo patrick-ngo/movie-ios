4954014eba487605efb88a62bd272fd8dcf2ff5f
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-18 00:55:54 +0800

    revamp MovieDetailVC into a TableView with a main header and a list of related movies

--- movie-ios/ViewControllers/MovieDetailVC.swift
+++ movie-ios/ViewControllers/MovieDetailVC.swift
@@ -12,223 +12,155 @@ import ReSwift
 import RxSwift
 import RxCocoa
 
-class MovieDetailVC: UIViewController, StoreSubscriber {
+class MovieDetailVC: UIViewController, UITableViewDelegate, UITableViewDataSource, StoreSubscriber {
+    enum Section : Int {
+        case header = 0
+        case related
+        case numberOfSections
+    }
+    
     typealias StoreSubscriberStateType = MovieDetailState
     
     let disposeBag = DisposeBag()
     
-    var movieId: Int? = nil
-    
-    private let locale = NSLocale(localeIdentifier: NSLocale.current.languageCode!)
-    
-    private var movie: MovieModel? = nil {
+    private var selectedMovie: MovieModel? = nil {
         didSet {
-            guard let movie = movie else { return }
-            
-            // Poster
-            if let profilePic = movie.poster_path {
-                let imageUrl = URL(string: "\(MoviesAPI.BASE_URL_IMAGES_HIGH)\(profilePic)")
-                self.posterImageView.sd_setImage(with: imageUrl, placeholderImage: nil)
-            }
-            
-            // Title
-            if let name = movie.title {
-                self.titleLabel.text = name
-                
-                // Also change navigation bar title
-                self.title = name
-            }
-            
-            if let date = movie.release_date {
-                // format: 2019-06-17
-                let index = date.index(date.startIndex, offsetBy: 4)
-                let yearSubstring = date[..<index]
-                self.yearLabel.text = String(yearSubstring)
-            }
-            
-            // Synopsys
-            if let overview = movie.overview {
-                self.synopsysLabel.text = overview
-            }
-            
-            // Genres
-            if let genres = movie.genre_ids, genres.count > 0 {
-                // Match genre ids with genre names
-                let genreNames = genres.map { (genreId) -> String in
-                    if let genre = Constants.Genres[genreId] {
-                        return genre
-                    }
-                    return ""
-                }
-                self.genresLabel.text = genreNames.joined(separator: " • ")
-            }
-            
-            // Language
-            if let original_language = movie.original_language {
-                if let language = self.locale.displayName(forKey: NSLocale.Key.identifier, value: original_language) {
-                    self.languageLabel.text = "\(NSLocalizedString("LABEL_LANGUAGE", comment: "Language")) \(language)"
-                }
-            }
-            
-            // Runtime
-            if let runtime = movie.runtime {
-                self.languageLabel.text = "\(NSLocalizedString("LABEL_RUNTIME", comment: "Runtime")) \(runtime)m"
-            }
+            self.title = self.selectedMovie?.title
         }
     }
+    private var movieList: [MovieModel] = []
+    private var page = 0
+    private var isLoading =  false
+    private var hasNext = true
     
     //MARK: - Views -
     
-    let scrollView : UIScrollView = {
-        let sv = UIScrollView()
-        sv.backgroundColor = .white
-        sv.alwaysBounceVertical = true
-        return sv
-    }()
-    
-    let posterImageView : UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.clipsToBounds = true
-        return iv
-    }()
-    
-    let gradientView: UIView = {
-        let v = UIView(frame: CGRect(x: 0, y: 0, width: Int(UIScreen.main.bounds.width), height: 300))
-        v.addGradientAtBottom()
-        return v
-    }()
-    
-    let titleLabel:UILabel = {
-        let lbl = UILabel()
-        lbl.text = ""
-        lbl.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
-        lbl.textColor = UIColor.Text.darkGrey
-        lbl.numberOfLines = 0
-        return lbl
-    }()
-    
-    let synopsysLabel:UILabel = {
-        let lbl = UILabel()
-        lbl.text = ""
-        lbl.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.regular)
-        lbl.textColor = UIColor.Text.darkGrey
-        lbl.numberOfLines = 0
-        lbl.lineBreakMode = NSLineBreakMode.byWordWrapping
-        return lbl
-    }()
-    
-    let genresLabel:UILabel = {
-        let lbl = UILabel()
-        lbl.text = ""
-        lbl.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.light)
-        lbl.textColor = UIColor.Text.darkGrey
-        return lbl
-    }()
-    
-    let yearLabel:UILabel = {
-        let lbl = UILabel()
-        lbl.text = ""
-        lbl.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.light)
-        lbl.textColor = UIColor.Text.darkGrey
-        return lbl
-    }()
-    
-    let languageLabel:UILabel = {
-        let lbl = UILabel()
-        lbl.text = ""
-        lbl.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.light)
-        lbl.textColor = UIColor.Text.darkGrey
-        return lbl
-    }()
-    
-    let runtimeLabel:UILabel = {
-        let lbl = UILabel()
-        lbl.text = ""
-        lbl.font = UIFont.systemFont(ofSize: 12, weight: UIFont.Weight.light)
-        lbl.textColor = UIColor.Text.darkGrey
-        return lbl
+    private lazy var tableView : UITableView = {
+        let tv = UITableView(frame: CGRect.zero, style: .plain)
+        tv.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
+        tv.separatorColor = UIColor.Border.around
+        
+        tv.delegate = self
+        tv.dataSource = self
+        
+        // Cell registration
+        tv.register(MovieDetailHeaderView.self, forHeaderFooterViewReuseIdentifier: String(describing: MovieDetailHeaderView.self))
+        tv.register(MovieDetailSectionHeaderView.self, forHeaderFooterViewReuseIdentifier: String(describing: MovieDetailSectionHeaderView.self))
+        tv.register(MovieListingsCell.self, forCellReuseIdentifier: String(describing: MovieListingsCell.self))
+        tv.register(TableViewLoadingCell.self, forCellReuseIdentifier: String(describing: TableViewLoadingCell.self))
+        
+        // Cell size
+        tv.rowHeight = UITableView.automaticDimension
+        tv.estimatedRowHeight = 80
+        
+        // Rx item selection
+        tv.rx.itemSelected
+            .map { self.movieList[$0.row] }
+            .bind(onNext: { (movie) in
+                // Update selected movie
+                mainStore.dispatch(SetSelectedMovie(movie: movie))
+                
+                // Go to movie detail screen
+                let movieDetailVC = MovieDetailVC()
+                self.navigationController?.pushViewController(movieDetailVC, animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        return tv
     }()
     
     //MARK: - Init -
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupNavBar()
         self.setupViews()
-        self.loadData()
         
+        
+        // subscribe to state changes
         mainStore.subscribe(self) { subcription in
             subcription.select { state in state.movieDetailState }
         }
+        
+        // Fetch related movies
+        mainStore.dispatch(fetchRelatedMovies)
+    }
+    
+    func setupNavBar() {
+        guard let navBar = self.navigationController?.navigationBar else { return }
+        navBar.tintColor = .black
+        navBar.barTintColor = .white
+        navBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        self.navigationItem.title = NSLocalizedString("TITLE_NOW_PLAYING", comment: "Now Playing")
     }
     
     func setupViews() {
-        self.view.addSubview(self.scrollView)
-        self.scrollView.addSubview(self.gradientView)
-        self.scrollView.addSubview(self.posterImageView)
-        self.scrollView.addSubview(self.titleLabel)
-        self.scrollView.addSubview(self.synopsysLabel)
-        self.scrollView.addSubview(self.yearLabel)
-        self.scrollView.addSubview(self.genresLabel)
-        self.scrollView.addSubview(self.languageLabel)
-        self.scrollView.addSubview(self.runtimeLabel)
+        self.view.addSubview(self.tableView)
         
-        self.scrollView.snp.makeConstraints { (make) in
-            make.edges.equalTo(0)
-        }
-        
-        self.gradientView.snp.makeConstraints { (make) in
-            make.height.equalTo(300)
-            make.right.left.equalTo(self.view)
-        }
-        
-        self.posterImageView.snp.makeConstraints { (make) in
-            make.centerX.equalTo(self.scrollView.snp.centerX)
-            make.top.equalTo(0)
-            make.height.equalTo(300)
-        }
-        self.titleLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self.posterImageView.snp.bottom).offset(10)
-            make.left.right.equalTo(15)
-        }
-        
-        self.genresLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self.titleLabel.snp.bottom).offset(4)
-            make.left.right.equalTo(15)
-        }
-        
-        self.yearLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self.genresLabel.snp.bottom).offset(4)
-            make.left.right.equalTo(15)
-        }
-        
-        self.synopsysLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self.yearLabel.snp.bottom).offset(10)
-            make.centerX.equalToSuperview()
-            make.left.right.equalTo(15)
-        }
-        
-        self.languageLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self.synopsysLabel.snp.bottom).offset(10)
-            make.left.right.equalTo(15)
-        }
-        
-        self.runtimeLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(self.languageLabel.snp.bottom).offset(10)
-            make.left.right.equalTo(15)
-            make.bottom.equalToSuperview().offset(-30)
+        self.tableView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalTo(0)
+            make.top.equalToSuperview()
         }
     }
     
     //MARK: - State Updates -
     func newState(state: MovieDetailState) {
         if let selectedMovie = state.selectedMovie {
-            self.movie = selectedMovie
+            self.selectedMovie = selectedMovie
+        }
+        
+        if let relatedMovies = state.relatedMovies {
+            self.movieList = relatedMovies
+            
+            // Reload table with new movies
+            self.tableView.reloadData()
+        }
+    }
+    
+    //MARK: - TableView Datasource -
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case Section.related.rawValue:
+            return movieList.count
+        default:
+            return 0
         }
     }
     
     
-    func loadData() {
-        mainStore.dispatch(fetchRelatedMovies)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case Section.header.rawValue:
+            return UITableViewCell(frame: CGRect.zero)
+            
+        case Section.related.rawValue:
+            // Get movie cell
+            let movieCell = self.tableView.dequeueReusableCell(withIdentifier: String(describing: MovieListingsCell.self)) as? MovieListingsCell
+            let movie = movieList[indexPath.row]
+            movieCell?.movie = movie
+            return movieCell!
+        default:
+            return UITableViewCell(frame: CGRect.zero)
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.numberOfSections.rawValue
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        switch section {
+        case Section.header.rawValue:
+            let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: String(describing:MovieDetailHeaderView.self)) as! MovieDetailHeaderView
+            headerView.movie = self.selectedMovie
+            return headerView
+        case Section.related.rawValue:
+            let sectionheaderView = tableView.dequeueReusableHeaderFooterView(withIdentifier: String(describing:MovieDetailSectionHeaderView.self)) as! MovieDetailSectionHeaderView
+            return sectionheaderView
+        default:
+            return UIView(frame: CGRect.zero)
+        }
     }
 }
+
