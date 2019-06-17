e2f4f6e4a35563ccc6ae35ed8349f2de1c204284
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-17 23:16:13 +0800

    add Constants file with mappings of genreId to genre name

---
+++ movie-ios/Constants/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  movie-ios
+//
+//  Created by Patrick Ngo on 2019-06-17.
+//  Copyright © 2019 Patrick Ngo. All rights reserved.
+//
+
+class Constants {
+    static let Genres: [Int: String] = [
+        28: "Action",
+        12: "Adventure",
+        16: "Animation",
+        35: "Comedy",
+        80: "Crime",
+        99: "Documentary",
+        10751: "Family",
+        14: "Fantasy",
+        36: "History",
+        27: "Horror",
+        10402: "Music",
+        9648: "Mystery",
+        10749: "Romance",
+        878: "Science Fiction",
+        10770: "TV Movie",
+        53: "Thriller",
+        10752: "War",
+        37: "Western",
+    ]
+}
+
