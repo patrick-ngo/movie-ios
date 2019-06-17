fe39f3e306ecb5f1d12c9ee4f021229be77a7f3a
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-18 00:49:56 +0800

    create SectionHeaderView

---
+++ movie-ios/ViewControllers/Views/MovieDetailSectionHeaderView.swift
@@ -0,0 +1,42 @@
+//
+//  MovieDetailSectionHeaderView.swift
+//  movie-ios
+//
+//  Created by Patrick Ngo on 2019-06-18.
+//  Copyright © 2019 Patrick Ngo. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class MovieDetailSectionHeaderView: UITableViewHeaderFooterView {
+    
+    lazy var titleLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.regular)
+        lbl.textColor = UIColor.Text.darkGrey
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 1
+        lbl.text = "Related Movies"
+        
+        return lbl
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier:reuseIdentifier)
+        
+        self.contentView.backgroundColor = UIColor.Background.grey
+        self.contentView.addSubview(self.titleLabel)
+        
+        self.titleLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.top.bottom.equalTo(0)
+            make.height.equalTo(36)
+        }
+    }
+}
