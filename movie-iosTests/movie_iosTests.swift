dbde535680e083696848c40cbadc0b9fa67a8fcc
Patrick Ngo <patrick.ngo@hotmail.com>
2019-06-18 01:06:06 +0800

    add some tests for the actions and reducers

--- movie-iosTests/movie_iosTests.swift
+++ movie-iosTests/movie_iosTests.swift
@@ -6,29 +6,85 @@
 //  Copyright © 2019 Patrick Ngo. All rights reserved.
 //
 
+import ReSwift
 import XCTest
 @testable import movie_ios
+
+struct EmptyAction: Action { }
 
 class movie_iosTests: XCTestCase {
 
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testInitialState() {
+        let state = appReducer(action: EmptyAction(), state: nil)
+        XCTAssertEqual(state, AppState())
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testStartFetchingMovies() {
+        let action = SetStartFetchingMovies(isFetchingMovies: true)
+        
+        let state = appReducer(action: action, state: nil)
+        
+        guard state.movieListState.isFetchingMovies == true else {
+            return XCTFail()
         }
     }
-
+    
+    
+    func testSetSelectedMovie() {
+        let movie = MovieModel(vote_count: nil, id: 12345, vote_average: 2.7573, title: "Rambo", popularity: 1.563, poster_path: nil, original_language: "en", original_title: "Rambo", genres: nil, genre_ids: nil, backdrop_path: nil, adult: true, overview: "Rambo first blood", release_date: nil, runtime: 122)
+        
+        let action = SetSelectedMovie(movie: movie)
+        
+        let state = appReducer(action: action, state: nil)
+        
+        guard state.movieDetailState.selectedMovie == movie else {
+            return XCTFail()
+        }
+    }
+    
+    func testSetMovies() {
+        let movies = [
+            MovieModel(vote_count: nil, id: 12345, vote_average: 2.7573, title: "Rambo", popularity: 1.563, poster_path: nil, original_language: "en", original_title: "Rambo", genres: nil, genre_ids: nil, backdrop_path: nil, adult: true, overview: "Rambo: First Blood", release_date: nil, runtime: 122),
+            MovieModel(vote_count: nil, id: 12346, vote_average: 1.323, title: "Terminator", popularity: 3.123, poster_path: nil, original_language: "en", original_title: "Terminator", genres: nil, genre_ids: nil, backdrop_path: nil, adult: true, overview: "Terminator: Judgement Day", release_date: nil, runtime: 122)
+        ]
+        
+        let action = SetMovies(movies: movies, page: 2, hasNext: true)
+        
+        let state = appReducer(action: action, state: nil)
+        
+        guard state.movieListState.movies == movies else {
+            return XCTFail()
+        }
+        guard state.movieListState.currentPage == 2 else {
+            return XCTFail()
+        }
+        guard state.movieListState.hasNext == true else {
+            return XCTFail()
+        }
+    }
+    
+    func testSetRelatedMovies() {
+        let movies = [
+            MovieModel(vote_count: nil, id: 12345, vote_average: 2.7573, title: "Rambo", popularity: 1.563, poster_path: nil, original_language: "en", original_title: "Rambo", genres: nil, genre_ids: nil, backdrop_path: nil, adult: true, overview: "Rambo: First Blood", release_date: nil, runtime: 122),
+            MovieModel(vote_count: nil, id: 12346, vote_average: 1.323, title: "Terminator", popularity: 3.123, poster_path: nil, original_language: "en", original_title: "Terminator", genres: nil, genre_ids: nil, backdrop_path: nil, adult: true, overview: "Terminator: Judgement Day", release_date: nil, runtime: 122)
+        ]
+        
+        let action = SetRelatedMovies(relatedMovies: movies)
+        
+        let state = appReducer(action: action, state: nil)
+        
+        guard state.movieDetailState.relatedMovies == movies else {
+            return XCTFail()
+        }
+    }
 }
